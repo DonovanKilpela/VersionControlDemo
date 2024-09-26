a6df28cb86fcf50083e080de6cd58b8263e01631
Donovan Kilpela <161644962+DonovanKilpela@users.noreply.github.com>
2024-09-25 22:42:35 -0500

    Adding basic Hello World to GitHub for assigment

---
+++ src/versioncontrol_donovankilpela/VersionControl_DonovanKilpela.java
@@ -0,0 +1,16 @@
+package versioncontrol_donovankilpela;
+
+/**
+ *
+ * @author donov
+ */
+public class VersionControl_DonovanKilpela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
